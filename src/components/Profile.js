d9cbafd166e30fb0fcc9a8f9568f406987eebf6b
lB4Ul <anish.sarkar.official@gmail.com>
2022-02-05 23:09:52 +0530

    Changed Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -24,7 +24,7 @@ const Profile = () => {
                     console.log(err)
                 })
         else {
-            alert('Post Id not found')
+            alert('Profile Id not found')
             history.replace("/")
         }
     }, [])
